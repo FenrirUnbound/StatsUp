44f6321605c20d5fbb5b8a845c8e246c39d2ed17
Darren M <aeneascorrupt@gmail.com>
2012-09-06 14:54:34 -0700

    Added application-specific constants

---
+++ lib/const.py
@@ -0,0 +1,14 @@
+class Const:
+    SPREADSHEET_NAME = 'FootballStats'
+    OFFENSE_PAGE_ONE = 'Offense1'
+    OFFENSE_PAGE_TWO = 'Offense2'
+    DEFENSE_PAGE_ONE = 'Defense1'
+    DEFENSE_PAGE_TWO = 'Defense2'    
+
+    START_ROW = 2
+
+    # Column numbers
+    FIRST_DOWN_COL = 3
+    NAME_COL = 2
+    PASS_COL = 5
+    RUSH_COL = 7
