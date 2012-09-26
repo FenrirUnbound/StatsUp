87fa9d503392c13ada377ed130be231a1336d567
Darren M <aeneascorrupt@gmail.com>
2012-09-25 19:01:52 -0700

    Customized formatting of pool data

--- views/scripts/pool.js
+++ views/scripts/pool.js
@@ -1,11 +1,14 @@
 var AWAY_NAME = 4,
     AWAY_SCORE = 5,
+    AWAY_TEAM = 0,
     GAME_CLOCK = 3,
+    GAME_MARGIN = 1,
     GAME_START_DAY = 0,
     GAME_START_TIME = 1,
     GAME_STATUS = 2,
     HOME_NAME = 6
     HOME_SCORE = 7,
+    HOME_TEAM = 1,
     NAMES = {
       'ARI': 'Arizona',
       'ATL': 'Atlanta',
@@ -39,7 +42,11 @@ var AWAY_NAME = 4,
       'TB': 'Tampa Bay',
       'TEN': 'Tennessee',
       'WAS': 'Washington'
-    };
+    },
+    TEAM_NAME = 0;
+
+var scoreboard_ = {},
+    spread_ = {};
 
 $(document).ready(function() {
   var scoreboardUrl = 'http://matsumoto26sunday.appspot.com/scoreboard',
@@ -47,12 +54,37 @@ $(document).ready(function() {
       templateName = '',
       templates = $('script[data-jsv-tmpl]');
 
+  // Get spread data
+  $.when($.get(spreadUrl))
+      .done(function(data) {
+          var element = '',
+              players = {};
+
+          spread_ = formatSpread_(data); //Save spread data
+
+          players = Object.keys(data).sort().reverse();
+          for(var i = players.length - 1; i >= 0; i -= 1) {
+            element += $.render.tmpl_listoption({
+              'name': players[i],
+              'value': players.length-i    
+            });
+          }
+          
+          // Load the selector
+          $('#selectSpread').html(element);
+          // Enable the select button
+          $('#selectButton').click(engageSpread_);
+          
+      });
+
   // Get scoreboard
   $.when($.get(scoreboardUrl))
       .done(function(data) {
         var current = {},
             gameScoreData = ($.parseJSON(data))['ss'].reverse(),
             result = {'scores': []};
+
+        scoreboard_ = gameScoreData;
 
         for(var i = gameScoreData.length - 1; i >= 0; i -= 1) {
           current = gameScoreData[i];
@@ -74,26 +106,72 @@ $(document).ready(function() {
         );
       });
 
-  // Get spread data
-  $.when($.get(spreadUrl))
-      .done(function(data) {
-          var element = '',
-              players = {};
-
-          players = Object.keys(data).sort().reverse();
-          for(var i = players.length - 1; i >= 0; i -= 1) {
-            element += $.render.tmpl_listoption({
-              'name': players[i],
-              'value': players.length-i    
-            });
-          }
-          
-          $('#selectSpread').html(element);
-      });
-
   // Load templates from DOM
   for(var i = templates.length - 1; i >= 0; i -= 1) {
     templateName = $(templates[i]).attr('id');
     $.templates(templateName, templates[i]);
   }
+});
+
+var engageSpread_ = (function() {
+  var awayTeam,
+      current,
+      homeTeam,
+      person = $('#selectSpread').find('option:selected').text(),
+      scores = $('#gameScores > ul > li > article');
+  
+  for(var i = scores.length - 1; i >= 0; i -= 1) {
+    // Extra away team
+    current = $(scores[i]).find('ul');
+    awayTeam = $(current[AWAY_TEAM]);
+    console.log(awayTeam);
+  }
+});
+
+/**
+ * Format the spread to guarantee tuplets of Name,Margin,Score
+ */
+var formatSpread_ = (function(spread) {
+  var current = [],
+      players = [],
+      result = {},
+      working;
+  
+  players = Object.keys(spread);
+  for(var i = players.length - 1; i >= 0; i -= 1) {
+    result[players[i]] = [];
+
+    current = spread[players[i]];
+    while(current.length > 0) {
+      working = current.splice(0, 3);
+      
+      if(working[GAME_MARGIN] == 'UN' || working[GAME_MARGIN] == 'OV') {
+        result[players[i]].push(working[0]);
+        result[players[i]].push(working[1]);
+        result[players[i]].push(working[2]);
+      }
+      else {
+        // First one is always a name
+        result[players[i]].push(working[0]);
+        result[players[i]].push(0);
+        result[players[i]].push(0);
+        
+        // If last set is game_margin, put everything else back
+        if(working[2] == 'UN' || working[2] == 'OV') {
+          current.unshift(working[2]);
+          current.unshift(working[1]);
+        }
+        else {
+          result[players[i]].push(working[1]);
+          result[players[i]].push(0);
+          result[players[i]].push(0);
+          
+          // No guarantees on last element; put back
+          current.unshift(working[2]);
+        } 
+      }
+    }
+  }
+
+  return result;
 });
