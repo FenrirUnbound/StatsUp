bcd5e681a334ebcc7ced4da48d38346430cebc2e
Darren M <aeneascorrupt@gmail.com>
2012-10-16 13:06:31 -0700

    Handled missing spreadsheets

--- spread.py
+++ spread.py
@@ -22,7 +22,6 @@ DAYS = {
     'SAT': 6,
     'SUN': 0
     }
-DEFAULT_WEEK = 6  #For testing purposes only
 DEFAULT_YEAR = 2012
 GAME_CLOCK = 3
 GAME_DAY = 0
@@ -47,7 +46,7 @@ class MainPage(webapp2.RequestHandler):
 
         # Check incoming parameters
         if week is None or len(week) == 0:
-            week = self._get_default_week()
+            week = self.get_current_week()
 
         scores = self._query_scores(week)
         # Check how stale the score is
@@ -73,10 +72,23 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Access-Control-Allow-Origin'] = '*'
         self.response.out.write(json.dumps(result, indent = 4))
 
+    def get_current_week(self):
+        week_one = datetime.datetime(2012, 9, 5, 0, 0, 0)
+        current = datetime.datetime.now()
+        
+        delta = current - week_one
+        
+        return ((delta.days / 7) + 1)
+
+    def get_season_year(self):
+        week_one = datetime.datetime(2012, 9, 5, 0, 0, 0)
+        
+        return week_one.year
+
     def _is_update_required(self, scores):
         if len(scores) > 0:
             #Shouldn't update if we're querying an archived week
-            if scores[0].week != self._get_default_week():
+            if scores[0].week != self.get_current_week():
                 return False
 
             today = datetime.datetime.now()
@@ -155,66 +167,83 @@ class MainPage(webapp2.RequestHandler):
         return result
 
     def _fetch_spread(self, spreadsheet, worksheet):
+        current_season = 'S' + str(self.get_season_year())
+        current_week = self.get_current_week()
+        data_sheets = []
         drive = Drive()
+        index = 0
         margin = {}
         odds = 0
         result = {}
         selections = {}
         spread = {}
+        target = ''
 
         # Default values
         if spreadsheet is None or len(spreadsheet) == 0:
-            spreadsheet = (drive.list_spreadsheets())[0]
+            if current_week < 10:
+                target = 'W0' + str(current_week)
+            else:
+                target = 'W' + str(current_week)
+
+            data_sheets = drive.list_spreadsheets()
+            try:
+                index = data_sheets.index(current_season + target)
+                spreadsheet = data_sheets[index]
+            except ValueError:
+                # No data sheet currently available
+                spreadsheet = ''
         if worksheet is None or len(worksheet) == 0:
             worksheet = 'Sheet1'
 
         data = drive.get_data(spreadsheet, worksheet)
         # Get each individual's spread choices
-        for i in data:
-            current = data[i]
-            header = current[0]
+        if len(data) > 0:
+            for i in data:
+                current = data[i]
+                header = current[0]
 
-            # Skip the labels
-            if 'GAME' in header or 'WEEK' in header:
-                continue
+                # Skip the labels
+                if 'GAME' in header or 'WEEK' in header:
+                    continue
 
-            # 1: offset. 3: season-long information
-            selections[header] = current[1+3:]
+                # 1: offset. 3: season-long information
+                selections[header] = current[1+3:]
 
-        # Parse for spread data
-        last_team = ''
-        over_under = 0
-        for team in data[2][5:]:
-            # Skip day-name labels
-            if 'DAY' in team:
-                continue
-            # Save the over/under margin
-            if 'OVER' in team:
-                index = team.index(' ')
+            # Parse for spread data
+            last_team = ''
+            over_under = 0
+            for team in data[2][5:]:
+                # Skip day-name labels
+                if 'DAY' in team:
+                    continue
+                # Save the over/under margin
+                if 'OVER' in team:
+                    index = team.index(' ')
 
-                over_under = int(team[index:-3])
-                over_under = (over_under + 0.5)
+                    over_under = int(team[index:-3])
+                    over_under = (over_under + 0.5)
                 
-                margin[last_team] = over_under
+                    margin[last_team] = over_under
                 
-                continue
-            # Skip total points tally
-            if 'TOTAL' in team:
-                continue
+                    continue
+                # Skip total points tally
+                if 'TOTAL' in team:
+                    continue
 
-            # Detect for underdog
-            if team[0] == '_':
-                spread[team[1:].upper()] = (odds * -1)
-            else:
-                #Format is: "TEAMNAME-12345 1/2"
-                deliminator = team.index('-')
+                # Detect for underdog
+                if team[0] == '_':
+                    spread[team[1:].upper()] = (odds * -1)
+                else:
+                    #Format is: "TEAMNAME-12345 1/2"
+                    deliminator = team.index('-')
 
-                odds = int(team[deliminator:-3])
-                odds = (odds - 0.5)
+                    odds = int(team[deliminator:-3])
+                    odds = (odds - 0.5)
                 
-                last_team = team[:deliminator].upper()
+                    last_team = team[:deliminator].upper()
                 
-                spread[last_team] = odds
+                    spread[last_team] = odds
 
         # Format the result
         result['spread'] = selections
@@ -240,14 +269,6 @@ class MainPage(webapp2.RequestHandler):
 
         return result
 
-    def _get_default_week(self):
-        week_one = datetime.datetime(2012, 9, 5, 0, 0, 0)
-        current = datetime.datetime.now()
-        
-        delta = current - week_one
-        
-        return ((delta.days / 7) + 1)
-
     def _query_scores(self, week):
         query = {}
         result = {}
@@ -271,7 +292,7 @@ class MainPage(webapp2.RequestHandler):
             # Completely new save
             for game in scores:
                 scorebox = Score(
-                    year = DEFAULT_YEAR,
+                    year = self.get_season_year(),
                     week = week,
                     away_name = game[AWAY_NAME].encode('ascii', 'ignore'),
                     away_score = int(game[AWAY_SCORE]),
