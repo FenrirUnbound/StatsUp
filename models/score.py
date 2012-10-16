05cd93de4d93d52f81ead86b4efc009c91a31421
Darren M <aeneascorrupt@gmail.com>
2012-10-15 19:30:18 -0700

    Added gameID field

---
+++ models/score.py
@@ -0,0 +1,15 @@
+from google.appengine.ext import db
+
+class Score(db.Model):
+    year = db.IntegerProperty()
+    week = db.IntegerProperty()
+    away_name = db.StringProperty()
+    away_score = db.IntegerProperty()
+    home_name = db.StringProperty()
+    home_score = db.IntegerProperty()
+    game_clock = db.StringProperty()
+    game_day = db.StringProperty()
+    game_status = db.StringProperty()
+    game_time = db.StringProperty()
+    game_id = db.IntegerProperty()
+    timestamp = db.DateTimeProperty()
