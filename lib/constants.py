efb88af09acb0a130d22340ceedf87c8d05dc308
Darren M <aeneascorrupt@gmail.com>
2012-11-07 20:31:37 -0800

    Additional constants

--- lib/constants.py
+++ lib/constants.py
@@ -1,5 +1,14 @@
 AWAY_NAME = 4
 AWAY_SCORE = 5
+DAYS = {
+    'MON': 1,
+    'TUE': 2,
+    'WED': 3,
+    'THU': 4,
+    'FRI': 5,
+    'SAT': 6,
+    'SUN': 0
+    }
 GAME_CLOCK = 3
 GAME_DAY = 0
 GAME_ID = 10
@@ -9,5 +18,8 @@ HTTP_OK = 200
 HOME_NAME = 6
 HOME_SCORE = 7
 SCORES_FETCHED = 'ss'
+THRESHOLD = 5
+TOTAL_TEAMS = 32
 URL_SCOREBOARD = 'http://www.nfl.com/liveupdate/scorestrip/scorestrip.json'
+UTC_OFFSET = -4
 YEAR = 2012
