0152d3bb8a2974da356a4b2d6e7a3b0ab7ca667f
Darren M <aeneascorrupt@gmail.com>
2012-11-07 20:32:00 -0800

    Allow score fetching & scores storing in db

--- scores.py
+++ scores.py
@@ -12,18 +12,23 @@ from models.score import Score
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        scores = {}
         result = {}
         to_save = self.request.get('save')
+        week = self.request.get('week')
 
-        scores = json.loads(self._fetch_scores())[constants.SCORES_FETCHED]
+        # Check incoming parameters
+        if week is None or len(week) == 0:
+            week = self._get_current_week()
 
-        if to_save is None or len(to_save) == 0:
-            result = scores
+        result = self._query_scores(week)
+        # Check how stale the score is
+        if (to_save is None or len(to_save) == 0 or
+                self._is_update_required(result)): 
+            result = json.loads(self._fetch_scores())[constants.SCORES_FETCHED]
+            self._save_scores(week, result)
         else:
-            week = self._get_default_week()
-            self._save_scores(week, scores)
-            result = {'success': 'true'}
+            # Format the data for client consumption
+            result = self._format_scores(result)
 
         self.response.headers['Content-Type'] = 'application/json'
         self.response.headers['Access-Control-Allow-Origin'] = '*'
@@ -64,13 +69,91 @@ class MainPage(webapp2.RequestHandler):
 
         return result
 
-    def _get_default_week(self):
+    def _format_scores(self, scores):
+        result = []
+        
+        for game in scores:
+            result.append([
+                game.game_day,
+                game.game_time,
+                game.game_status,
+                game.game_clock,
+                game.away_name,
+                str(game.away_score),
+                game.home_name,
+                str(game.home_score)
+            ])
+
+        return result
+
+    def _get_current_week(self):
         week_one = datetime.datetime(2012, 9, 6, 0, 0, 0)
         current = datetime.datetime.now()
         
         delta = current - week_one
         
         return ((delta.days / 7) + 1)
+
+    def _is_update_required(self, scores):
+        if len(scores) > 0:
+            #Shouldn't update if we're querying an archived week
+            if scores[0].week != self._get_current_week():
+                return False
+
+            today = datetime.datetime.now()
+            for game in scores:
+                # today is a gameday
+                if today.weekday() == constants.DAYS[game.game_day.upper()]:
+                    offset = 12   # !!! Assume all games are in the afternoon
+                    index = game.game_time.index(':')
+                    game_hour = int(game.game_time[:index]) + offset
+                    game_minute = int(game.game_time[(index+1):])
+
+                    # Check if the game has already started
+                    if game_hour > (today.hour + constants.UTC_OFFSET):
+                        # Don't fetch if the game is over
+                        if 'Final' in game.game_status:
+                            continue
+                        elif 'final' in game.game_status:
+                            continue
+
+                        # Check if timestamp is stale
+                        time_delta = today - game.timestamp
+                        if time_delta >= datetime.timedelta(
+                                minutes=constants.THRESHOLD
+                                ):
+                            return True
+                    elif game_hour == (today.hour + constants.UTC_OFFSET):
+                        if game_minute >= today.minute:
+                            # It's hard to believe a game is less than an hour
+                            # Check if timestamp is stale
+
+                            time_delta = today - game.timestamp
+                            threshold = datetime.timedelta(
+                                    minutes=constants.THRESHOLD
+                                    )
+                            if time_delta >= threshold:
+                                return True
+                else:
+                    # Only update if the timestamp is stale by at least a day
+                    if today.weekday() != game.timestamp.weekday():
+                        return True
+        else:
+            # Query set is empty
+            return True
+
+        return False
+
+    def _query_scores(self, week):
+        query = {}
+        result = {}
+
+        query = Score.all()
+        query.filter('week =', week)
+        query.order('game_id')
+        result = query.fetch(constants.TOTAL_TEAMS)
+        
+        return result
 
     def _save_scores(self, week, scores):
         query = Score.all()
@@ -86,14 +169,17 @@ class MainPage(webapp2.RequestHandler):
                 scorebox = Score(
                     year = constants.YEAR,
                     week = week,
-                    away_name = game[constants.AWAY_NAME].encode('ascii', 'ignore'),
+                    away_name = game[constants.AWAY_NAME].encode('ascii', 
+                                                                    'ignore'),
                     away_score = int(game[constants.AWAY_SCORE]),
                     game_clock = str(game[constants.GAME_CLOCK]),
-                    game_day = game[constants.GAME_DAY].encode('ascii', 'ignore'),
+                    game_day = game[constants.GAME_DAY].encode('ascii', 
+                                                                    'ignore'),
                     game_id = int(game[constants.GAME_ID]),
                     game_status = game[constants.GAME_STATUS],
                     game_time = game[constants.GAME_TIME],
-                    home_name = game[constants.HOME_NAME].encode('ascii', 'ignore'),
+                    home_name = game[constants.HOME_NAME].encode('ascii',
+                                                                    'ignore'),
                     home_score = int(game[constants.HOME_SCORE]),
                     timestamp = datetime.datetime.now()
                     )
