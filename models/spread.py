2197711ecc244c3bbf50dd0e1aaaa5366ab3e5e9
Darren M <aeneascorrupt@gmail.com>
2012-11-10 01:39:44 -0800

    Removed game_id column

--- models/spread.py
+++ models/spread.py
@@ -3,7 +3,6 @@ from google.appengine.ext import db
 class Spread(db.Model):
     year = db.IntegerProperty()
     week = db.IntegerProperty()
-    game_id = db.IntegerProperty()
     person = db.StringProperty()
     team_name = db.StringProperty()
     margin = db.IntegerProperty()
