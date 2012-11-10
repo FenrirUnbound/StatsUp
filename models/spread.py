be83be5869a8cc7310c3e3c6bce9a13730fc3672
Darren M <aeneascorrupt@gmail.com>
2012-11-09 19:14:03 -0800

    Added 'person' column to determine owner

--- models/spread.py
+++ models/spread.py
@@ -4,6 +4,7 @@ class Spread(db.Model):
     year = db.IntegerProperty()
     week = db.IntegerProperty()
     game_id = db.IntegerProperty()
+    person = db.StringProperty()
     team_name = db.StringProperty()
     margin = db.IntegerProperty()
     total_score = db.IntegerProperty()
