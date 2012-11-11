06c87d0427684a4655dd6fdf9820a9b8e14ef654
Darren M <aeneascorrupt@gmail.com>
2012-11-11 11:50:16 -0800

    Corrected the DAYS constant

--- lib/constants.py
+++ lib/constants.py
@@ -3,13 +3,13 @@ import datetime
 AWAY_NAME = 4
 AWAY_SCORE = 5
 DAYS = {
-    'MON': 1,
-    'TUE': 2,
-    'WED': 3,
-    'THU': 4,
-    'FRI': 5,
-    'SAT': 6,
-    'SUN': 0
+    'MON': 0,
+    'TUE': 1,
+    'WED': 2,
+    'THU': 3,
+    'FRI': 4,
+    'SAT': 5,
+    'SUN': 6
     }
 DEFAULT_WORKSHEET = 'Sheet1'
 GAME_CLOCK = 3
