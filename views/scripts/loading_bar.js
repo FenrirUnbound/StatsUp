0d794a75e5a38b6ca800e6e1e9d45c54c48ffcd6
Darren M <aeneascorrupt@gmail.com>
2012-11-24 18:12:55 -0800

    Cleaned up unnecessary code

--- views/scripts/loading_bar.js
+++ views/scripts/loading_bar.js
@@ -1,47 +1,33 @@
-var loading_bar = loading_bar || {};
+var loadingBar = loadingBar || {};
 
-loading_bar = (function() {
+loadingBar = (function() {
   var loadedFiles_ = 0,
       totalFiles_ = 0,
       value_ = 0;
 
   function init(totalFiles) {
+    loadedFiles_ = 0;
     totalFiles_ = totalFiles;
-  }
-
-  function run() {
-    return true;
+    value_ = 0;
   }
   
-  function loaded() {
+  function itemLoaded() {
     loadedFiles_ += 1;
     setValue_(loadedFiles_ * 100 / totalFiles_);
     
     // Are all the files loaded?
-    if(loadedFiles_ === totalFiles_)
-      setTimeout('loading_bar.hide()', 300);
+    if(loadedFiles_ === totalFiles_) {
+      setTimeout('loadingBar.hide()', 1000);
+    }
   }
   
   function hide() {
-    document.getElementById('loadingSection').style.visibility = 'hidden';
+    document.getElementById('sectionLoading').style.display = 'none';
   }
   
-  // Center in the screen
-  function locate_() {
-    var loadingZone = document.getElementById('loadingSection'),
-        popupHeight = loadingZone.clientHeight,
-        popupWidth = loadingZone.clientWidth,
-        windowHeight = document.documentElement.clientHeight,
-        windowWidth = document.documentElement.clientWidth;
-        
-    loadingZone.style.position = 'absolute';
-    loadingZone.style.top = parseInt(windowHeight/2-popupHeight/2) + 'px';
-    loadingZone.style.left = parseInt(windowWidth/2-popupWidth/2) + 'px';
-  }
-  
-  // Set the bottom text value
-  function setAction_(action) {
-    document.getElementById('loadingInfo').innerHTML = action;
+  function reset() {
+    init(totalFiles_);
+    setValue_(value_);
   }
   
   // Set the value position of the bar (Only 0-100 values allowed)
@@ -55,7 +41,6 @@ loading_bar = (function() {
   return {
     'hide': hide,
     'init': init,
-    'loaded': loaded,
-    'run': run
+    'itemLoaded': itemLoaded
   }
 })();
